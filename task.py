16c1fd30c2727d22a52b9616ea3d88a67e6fdb07
Ruvan Leslie <stampibot@gmail.com>
2025-04-23 10:05:09 +0200

    Add files via upload

--- task.py
+++ task.py
@@ -1,48 +1,28 @@
-from random import choice
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M or L: ")
+pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
+extra_cheese = input("Do you want extra cheese? Y or N: ")
 
-print(r'''
-*******************************************************************************
-          |                   |                  |                     |
- _________|________________.=""_;=.______________|_____________________|_______
-|                   |  ,-"_,=""     `"=.|                  |
-|___________________|__"=._o`"-._        `"=.______________|___________________
-          |                `"=._o`"=._      _`"=._                     |
- _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
-|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
-|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
-          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
- _________|___________| ;`-.o`"=._; ." ` '`."\ ` . "-._ /_______________|_______
-|                   | |o ;    `"-.o`"=._``  '` " ,__.--o;   |
-|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
-____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
-/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
-____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
-/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
-____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
-/______/______/______/______/______/______/______/______/______/______/_____ /
-*******************************************************************************
-''')
-print("Welcome to Treasure Island.")
-print("Your mission is to find the treasure.")
-choice1=input('You\'re at a crossroad, where dp you want to go? Type "left" or "right".').lower()
+# todo: work out how much they need to pay based on their size choice.
+bill = 0
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+elif size == "L":
+    bill += 25
+else:
+    print("You typed the wrong inputs.")
 
-if choice1 == "left":
-    choice2 =input('You\'ve come to a lake,there is an island in the middle of the lake. '
-                   'Type "wait" yo wait for a boat.'
-                   ' Type "swim" tp swim across.').lower()
-    if choice2 == "wait":
-        choice3 = input("You arrive at the island unharmed."
-                        " There is house with 3 doors."
-                        " One red, one yellow, and one blue."
-                        " Which colour do you choose?").lower()
-        if choice3 == "red":
-            print("It's a room full of fire. Game Over")
-        elif choice3 == "yellow":
-            print("You found the treasure. You win")
-        elif choice3 == "blue":
-            print("You enter a room full of zombies. Game Over")
-        else:
-          print("You chose a door that does not exist. Game Over ")
-if choice1 =="right":
-    print("You fell in to a hole! Game over")
+# todo: work out how much to add to their bill based on their pepperoni choice.
+if pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
 
+# todo: work out their final amount based on whether if they want extra cheese.
+if extra_cheese == "Y":
+    bill += 1
+
+print(f"Your final bill is : ${bill}.")
